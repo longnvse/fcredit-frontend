c7a98a47804c86dc3c32f7178615f4d0a355ea9a
Tran Anh Quang <dinosaurous9x@gmail.com>
2023-02-20 00:53:44 +0700

    test

--- src/pages/logout.js
+++ src/pages/logout.js
@@ -1,1 +1,12 @@
 import React from 'react';
+const Logout = (props) => {
+    return (
+        <div>
+            Logout {props.name}
+        </div>
+    );
+};
+
+Login.propTypes = {};
+
+export default Login;
