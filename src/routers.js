9f238e9cfaafa51c260c26410f00d46309822b63
Tran Anh Quang <dinosaurous9x@gmail.com>
2023-03-13 21:24:33 +0700

    deb-list

--- src/routers.js
+++ src/routers.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import VerifyUser from "./pages/verify-user";
 import ListDebtor from "./pages/debtor";
+import DebtMenu from './pages/debtor/menu';
 
 const Routers = (props) => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path={"/"} element={<ListDebtor/>}/>
-                <Route path={"/login"} element={<Login/>}/>
-                <Route path={"/register"} element={<Register/>}/>
-                <Route path={"/verify-user"} element={<VerifyUser/>}/>
+                <Route path={"/menu"} element={<DebtMenu />} />
+                <Route path={"/debt-list"} element={<ListDebtor />} />
+                <Route path={"/login"} element={<Login />} />
+                <Route path={"/register"} element={<Register />} />
+                <Route path={"/verify-user"} element={<VerifyUser />} />
             </Routes>
         </BrowserRouter>
     );
